11feac314b21434fc386e407e2631b1780d2c47c
Hunter <8601934+judge2020@users.noreply.github.com>
2020-03-21 16:16:52 -0400

    Script for updating repo

---
+++ do.ts
@@ -0,0 +1,26 @@
+import { Octokit } from "@octokit/rest";
+const octokit = new Octokit();
+
+(async () => {
+    let release = await octokit.repos.getLatestRelease({ owner: "CorporateClash", repo: "pyside2-releases" });
+    let _assets = release.data.assets.filter(asset => asset.name == "installer.exe")
+    if (_assets.length != 1) {
+        console.log("Error: no installer.exe asset in the latest release on GitHub.")
+        process.exit(1)
+    }
+    let installer = _assets[0]
+    console.log(installer.browser_download_url)
+    if (process.env.GITHUB_ACTIONS) {
+        octokit.repos.createOrUpdateFile({
+            owner: "CorporateClash",
+            repo: "pyside2-releases",
+            path: "latest.txt",
+            message: "Update latest release url", // commit message
+            content: installer.browser_download_url
+        })
+    }
+    else {
+        console.log("Not running in GH actions, exiting")
+    }
+    process.exit(0)
+})()
